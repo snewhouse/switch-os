64a9029b3c3fac30ddf69baed31e01fbb3b1d7b0
Stephen J Newhouse <stephen.j.newhouse@gmail.com>
2017-05-01 15:25:43 +0100

    Update switch-os.sh

--- switch-os.sh
+++ switch-os.sh
@@ -3,14 +3,19 @@
 # Run Docker based OS on Host OS
 # Inspired by: https://github.com/wurmlab/oswitch
 
-VERSION="17.05.0.1" ## YY.MM.VV
+VERSION="17.05.0.1-alpha" ## YY.MM.VV
 AUTHOR="Stephen Newhouse <stephen.j.newhouse@gmail.com>"
 
 # select Docker OS
 if [[ "${1}" == "list" ]]; then
-    echo -e "Available OS's:
+    echo -e "
+    ------------------------
+    [switch-os ]: Seamlessly switch between Os's using Docker
+    [Version   ]: ${VERSION}
+    ------------------------
+    Available OS's:
      - ubuntu:16.04     [Usage: switch-os.sh ubuntu   ]
-     - yeban/biolinux:8 [Usage: switch-os.sh biolinux ]"
+     - yeban/biolinux:8 [Usage: switch-os.sh biolinux ]\n"
      exit 1
 elif [[ "${1}" == "ubuntu" ]]; then
     DOCKER_OS="ubuntu:16.04"
